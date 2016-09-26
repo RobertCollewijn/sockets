4916f2d4060be7ad1319481a2f959f232711bbd2
Robert <robert@tooconnect.nl>
2016-09-26 12:16:52 +0200

    added client message event

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,8 @@ var socket = io();
 socket.on('connect',function () {
     console.log("connected to socket.io server")
 })
+
+socket.on('message',function (message) {
+    console.log('new message');
+    console.log((message.text));
+})
