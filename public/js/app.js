1d6f77b476f94ad2669e2af81a0b0002bfce9a8a
Robert <robert@tooconnect.nl>
2016-09-27 10:20:49 +0200

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -5,36 +5,36 @@ var socket = io();
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 
-console.log(name+' wants to join room ' + room);
+console.log(name + ' wants to join room ' + room);
 jQuery('.room-title').html(room);
-socket.on('connect',function () {
+socket.on('connect', function () {
     console.log("connected to socket.io server");
-    socket.emit('joinRoom',{
+    socket.emit('joinRoom', {
         name: name,
         room: room
     })
 })
 
-socket.on('message',function (message) {
+socket.on('message', function (message) {
     console.log('new message');
     var timestampMoment = moment.utc(message.timestamp);
-    var $message = jQuery('.messages');
-
-    $message.append('<p><strong>'+ message.name + ' ' +  timestampMoment.local().format('HH:mm:ss') + ': </strong>');
-    $message.append('<p>'  + message.text + '</p>')
-
+    var $messages = jQuery('.messages');
+    var $message = jQuery('<li class="list-group-item"> </li>');
+    $message.append('<p><strong>' + message.name + ' ' + timestampMoment.local().format('HH:mm:ss') + ': </strong>');
+    $message.append('<p>' + message.text + '</p>');
+    $   messages.append($message);
 })
 
 // handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit',function (event) {
+$form.on('submit', function (event) {
     event.preventDefault();
 
     var $message = $form.find('input[name=message]');
-    socket.emit('message',{
-        name:name,
-        text:$message.val()
+    socket.emit('message', {
+        name: name,
+        text: $message.val()
     });
 
     $message.val("")
